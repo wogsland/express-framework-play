01aaa39d2641e273dd0dc8ee19777f7946741674
Bradley Wogsland <bwogsland@criticalmix.com>
2017-03-22 11:19:31 -0500

    routing & static files

--- myapp/app.js
+++ myapp/app.js
@@ -1,8 +1,18 @@
 var express = require('express')
 var app = express()
 
+app.use(express.static('public'))
+
 app.get('/', function (req, res) {
-  res.send('Hallo Welt!')
+  res.send('Hallo Welt! <a href="/awesome">Awesomeness</a> and <a href="/camping">Camping</a>.')
+})
+
+app.get('/awesome', function (req, res) {
+  res.send('This page is awesome!')
+})
+
+app.get('/camping', function (req, res) {
+  res.send('<img src="images/IMG_20170303_193711.jpg" width="600" />')
 })
 
 app.listen(1337, function () {
